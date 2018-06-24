975b12e3918f355099aaadd3e0b0dee7f2e8b35c
GTHell <sosxradar@gmail.com>
2018-06-24 15:05:59 +0700

    Initialize complete template of HTML/JS/CSS with webpack HRM

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,42 @@
 const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWepbackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: {
     app: './src/index.js'
   },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [
+        'babel-loader'
+      ]
+    },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWepbackPlugin({
+      title: 'CSS-GRID',
+      template: 'src/index.html'
+    })
+  ],
   output: {
-    filename: '[name].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
